26ddc85dc9ccd3c831367ac662e200f2aa8169d3
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-01-31 21:15:58 +0200

    I'm designing the AboutPageComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { DesignPageComponent } from './pages/design-page/design-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   {path: "", component: HomePageComponent},
+  {path: "about", component: AboutPageComponent},
   {path: "design", component: DesignPageComponent}
 ];
 
