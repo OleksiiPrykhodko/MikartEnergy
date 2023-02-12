3ad161b571f0f4ad32e382c3ea85e3d391bab04b
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-02-12 19:40:41 +0200

    I am adding ngOnInit and ngOnDestroy for app.component to unsubscribe when it's destroyed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd} from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -10,30 +11,40 @@ import { filter } from 'rxjs/operators';
 export class AppComponent {
   title = 'MikartEnergy';
 
+  private _routerSubscription: Subscription = Subscription.EMPTY;
   private _routeNamePathPair: [routeName: string, routePath: string][] = [];
 
-  constructor(router:Router){
-    router.events.pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: any) => {
-        var currentRoute = event.url.substring(1);
-        if(currentRoute.length > 0){
-          currentRoute = currentRoute.split("/", 3);
-          var routeNamePathPair: [string, string][] = [];
-          for (let index = 0; index < currentRoute.length; index++) {
-            if(index === 0){
-              routeNamePathPair.push([currentRoute[index], "/"+currentRoute[index]])
-            }
-            else{
-              routeNamePathPair.push([currentRoute[index], `${currentRoute[index - 1]}/${currentRoute[index]}`]);
-            }
-          }
-          this._routeNamePathPair = routeNamePathPair;
-        }
-      })
+  constructor(private router:Router){
   }
+
+  ngOnInit(){
+    this._routerSubscription = this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: any) => this.HandleNavigationEndEvent(event))
+  }
+
+  ngOnDestroy(){
+    this._routerSubscription.unsubscribe();
+   }
 
   public get RouteNamePathPair() : [routeName: string, routePath: string][] {
     return this._routeNamePathPair;
+  }
+
+  private HandleNavigationEndEvent(navigationEndEvent : any){
+    var currentRoute = navigationEndEvent.url.substring(1);
+      if(currentRoute.length > 0){
+        currentRoute = currentRoute.split("/", 3);
+        var routeNamePathPair: [string, string][] = [];
+        for (let index = 0; index < currentRoute.length; index++) {
+          if(index === 0){
+            routeNamePathPair.push([currentRoute[index], "/"+currentRoute[index]])
+          }
+          else{
+            routeNamePathPair.push([currentRoute[index], `${currentRoute[index - 1]}/${currentRoute[index]}`]);
+          }
+        }
+        this._routeNamePathPair = routeNamePathPair;
+      }
   }
 
 }
