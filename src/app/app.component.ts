f75dd53de6dffa66cf9af6d4f08ec6a288264a27
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-02-12 14:29:07 +0200

    I have added routing display logic for navbar to the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// Routing
+import { Router, NavigationEnd} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MikartEnergy';
+
+  private _currentRoute: string[] = [];
+
+  constructor(router:Router){
+    router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        var currentRoute = event.url.substring(1);
+        if(currentRoute.length > 0){
+          this._currentRoute = currentRoute.split("/", 3);
+        }
+      })
+  }
+
+  public get CurrentRoute() : string[] {
+    return this._currentRoute;
+  }
+
 }
