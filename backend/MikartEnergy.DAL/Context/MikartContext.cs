bf4da5b4ccfae705089ac2d6ad279bc8ddee450b
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-05-01 18:48:15 +0300

    I have created DB Context and extention class for base seeding.

---
+++ backend/MikartEnergy.DAL/Context/MikartContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MikartEnergy.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.DAL.Context
+{
+    internal class MikartContext : DbContext
+    {
+        public DbSet<CallbackRequest> callbackRequests { get; private set; }
+
+        public MikartContext(DbContextOptions<MikartContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            try
+            {
+                // Setting up entities using extension method.
+                modelBuilder.Configure();
+
+                // Seeding data using extension method.
+                // TODO: Is this method will be called every time after adding a new migration? 
+                modelBuilder.Seed();
+            }
+            catch (System.Exception e)
+            {
+                var text = e.Message;
+                throw;
+            }
+        }
+    }
+}
---
+++ backend/MikartEnergy.DAL/Context/ModelBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MikartEnergy.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.DAL.Context
+{
+    public static class ModelBuilderExtensions
+    {
+        // Specific configurations for each entity.
+        public static void Configure(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CallbackRequest>().Property(p => p.Id).HasDefaultValueSql("NEWID()");
+        }
+
+        // DB seeding.
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
