178de3018bf4c6e3d91b79c2e5f2c10b86b2590f
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-05-01 18:43:47 +0300

    I've added base entity class.

---
+++ backend/MikartEnergy.DAL/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MikartEnergy.DAL.Entities.Abstract
+{
+    public abstract class BaseEntity
+    {
+        private DateTime _createdAt;
+
+        public BaseEntity()
+        {
+            CreatedAt = UpdatedAt = DateTime.Now;
+        }
+
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+            set { _createdAt = (value == DateTime.MinValue) ? DateTime.Now : value; }
+        }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
