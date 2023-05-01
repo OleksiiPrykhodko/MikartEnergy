31869bcf132d9b7d245b47566f9aa0c1b6bb16e4
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-05-01 18:45:21 +0300

    I have added CallbackRequest class.

---
+++ backend/MikartEnergy.DAL/Entities/CallbackRequest.cs
@@ -0,0 +1,20 @@
+﻿using MikartEnergy.DAL.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.DAL.Entities
+{
+    public class CallbackRequest : BaseEntity
+    {
+        public string AuthorName { get; set; }
+        public string AuthorEmail { get; set; }
+        public string AuthorPhone { get; set; }
+        public string Message { get; set; }
+        public string IntrerestedIn { get; set; }
+        public decimal Budget { get; set; }
+        public bool InWork { get; set; }
+    }
+}
