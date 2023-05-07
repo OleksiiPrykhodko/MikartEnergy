0be9b28fcc7c32e7bd55b5f90a8546a4f9a14cbc
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-05-07 12:30:31 +0300

    I add an interface to indicate errors in the DTO and apply this interface.

---
+++ backend/MikartEnergy.Common/DTO/Abstract/IResponseStatusDTO.cs
@@ -0,0 +1,15 @@
+﻿using MikartEnergy.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.Common.DTO.Abstract
+{
+    public interface IResponseStatusDTO
+    {
+        bool Successful { get; set; }
+        KeyValuePair<ResponseErrors, string>[] Errors { get; set; }
+    }
+}
--- backend/MikartEnergy.Common/DTO/CallbackRequest/CallbackRequestDTO.cs
+++ backend/MikartEnergy.Common/DTO/CallbackRequest/CallbackRequestDTO.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿using MikartEnergy.Common.DTO.Abstract;
+using MikartEnergy.Common.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MikartEnergy.Common.DTO.CallbackRequest
 {
-    public class CallbackRequestDTO
+    public class CallbackRequestDTO : IResponseStatusDTO
     {
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public bool InWork { get; set; }
         public bool IsDeleted { get; set; }
+
+        public bool Successful { get; set; } = true;
+        public KeyValuePair<ResponseErrors, string>[] Errors { get; set; }
 
         public string AuthorName { get; set; }
         public string AuthorEmail { get; set; }
