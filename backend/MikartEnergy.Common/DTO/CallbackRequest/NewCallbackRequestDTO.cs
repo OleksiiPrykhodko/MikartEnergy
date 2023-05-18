481ec59dd21a77be7210a387b781fc40ef158a23
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-05-18 11:20:31 +0300

    I inherit the ResponseStatusDTO class in the NewCallbackRequestDTO class.

--- backend/MikartEnergy.Common/DTO/CallbackRequest/NewCallbackRequestDTO.cs
+++ backend/MikartEnergy.Common/DTO/CallbackRequest/NewCallbackRequestDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MikartEnergy.Common.DTO.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MikartEnergy.Common.DTO.CallbackRequest
 {
-    public class NewCallbackRequestDTO
+    public class NewCallbackRequestDTO : ResponseStatusDTO
     {
         public string AuthorName { get; set; }
         public string AuthorEmail { get; set; }
