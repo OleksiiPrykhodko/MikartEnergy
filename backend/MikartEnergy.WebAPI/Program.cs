cf32b0610fa410b79959f15c52a4903fa04727e8
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-05-31 17:40:37 +0300

    I've registered CORS.

--- backend/MikartEnergy.WebAPI/Program.cs
+++ backend/MikartEnergy.WebAPI/Program.cs
@@ -15,25 +15,38 @@ namespace MikartEnergy.WebAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
-            
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Add CORS.
+            builder.Services.AddCors();
 
             // Add DB Context.
             builder.Services.AddDbContext<MikartContext>(options => options.UseInMemoryDatabase("MikartInMemoryDB"));
 
-            // Add business logic services
+            // Add business logic services.
             builder.Services.RegisterCustomServices();
-            // Add FluentValidation 
+            // Add FluentValidation.
             builder.Services.RegisterCustomValidators();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
+                // Configure the HTTP request pipeline.
                 app.UseSwagger();
                 app.UseSwaggerUI();
+
+                // Allow any origin in dev mode.
+                app.UseCors(option => option.AllowAnyOrigin());
+            }
+            if (app.Environment.IsProduction())
+            {
+                // Allow only frontend origin in prodaction mode.
+                // TODO: specifie here frontend URL on production mode for CORS
+                app.UseCors(option => option.WithOrigins(""));
             }
 
             app.UseHttpsRedirection();
