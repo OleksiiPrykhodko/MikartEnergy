eb4f230f937928b815f07a7fc751d35a7a6e1945
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-06-07 15:58:15 +0300

    I've changed routing for new pages.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { DesignPageComponent } from './pages/design-page/design-page.component';
 import { DevelopmentPageComponent } from './pages/development-page/development-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
+import { ShopPageComponent } from './pages/shop-pages/shop-page/shop-page.component';
+import { ProductsPageComponent } from './pages/shop-pages/products-page/products-page.component';
+import { ProductPageComponent } from './pages/shop-pages/product-page/product-page.component';
 
 const routes: Routes = [
-  {path: "", component: HomePageComponent},
-  {path: "about", component: AboutPageComponent},
-  {path: "development", component: DevelopmentPageComponent},
-  {path: "contacts", component: ContactsPageComponent},
-  {path: "design", component: DesignPageComponent}
+  { path: "", component: HomePageComponent },
+  { path: "about", component: AboutPageComponent },
+  { path: "development", component: DevelopmentPageComponent },
+  { path: "contacts", component: ContactsPageComponent },
+  { path: "shop", component: ShopPageComponent},
+  { path: "shop/products", component: ProductsPageComponent },
+  { path: "shop/products/:idProduct", component: ProductPageComponent },
+  { path: "design", component: DesignPageComponent }
 ];
 
 @NgModule({
