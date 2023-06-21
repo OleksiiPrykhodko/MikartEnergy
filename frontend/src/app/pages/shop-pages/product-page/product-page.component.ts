39a7fbff15250bc6af5aef22a5bf2881c810e09e
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-06-21 18:01:14 +0300

    I added logic to deteсе the component ID in the product-page.component.

--- frontend/src/app/pages/shop-pages/product-page/product-page.component.ts
+++ frontend/src/app/pages/shop-pages/product-page/product-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product/product';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss']
 })
+
 export class ProductPageComponent {
+
+  public _infoLoaded: boolean = true;
+  public _productIdFromRoute: string = "";
+  private _subscriptionToRoutParamChange: Subscription;
+  private _product: Product;
+
+  constructor(private activateRoute: ActivatedRoute) {
+    this._subscriptionToRoutParamChange =
+      activateRoute.params.subscribe(params => this._productIdFromRoute = params["productID"]);
+  }
+
+  ngOnDestroy() {
+    this._subscriptionToRoutParamChange.unsubscribe();
+  }
 
 }
