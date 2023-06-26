8a796d6671cd183c3fe0899047f5abe15440865e
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-06-26 21:30:06 +0300

    I've created ProductMapper in backend.

---
+++ backend/MikartEnergy.BLL/Mapping/ProductMapper.cs
@@ -0,0 +1,68 @@
+﻿using MikartEnergy.Common.DTO.CallbackRequest;
+using MikartEnergy.Common.DTO.Product;
+using MikartEnergy.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.BLL.Mapping
+{
+    public static class ProductMapper
+    {
+        public static ProductDTO ToProductDTO(this Product entity)
+        {
+            return new ProductDTO
+            {
+                Id = entity.Id,
+                ManufacturerName = entity.ManufacturerName,
+                OrderNumber = entity.OrderNumber,
+                ProductName = entity.ProductName,
+                ShortDescription = entity.ShortDescription,
+                LongDescription = entity.LongDescription,
+                TechnicalData = entity.TechnicalData,
+
+                ImageLowQualityURL = entity.ImageLowQualityURL,
+                ImageHighQualityURL = entity.ImageHighQualityURL,
+                PdfWith3dURL = entity.PdfWith3dURL,
+
+                LinkToProductPage = entity.LinkToProductPage,
+                LinkToManuals = entity.LinkToManuals,
+                LinkToFAQ = entity.LinkToFAQ,
+                LinkToTechnicalData = entity.LinkToTechnicalData,
+                LinkToApplicationExample = entity.LinkToApplicationExample,
+                LinkToVideo = entity.LinkToVideo,
+
+                RelatedProductIDs = entity.RelatedProductIDs,
+
+                MinimalOrderQuantity = entity.MinimalOrderQuantity,
+                MaximalOrderQuantity = entity.MaximalOrderQuantity,
+                OrderQuantityMultiplier = entity.OrderQuantityMultiplier,
+                InStock = entity.InStock,
+                Price = entity.Price,
+                PriceCurrency = entity.PriceCurrency,
+            };
+        }
+
+        public static ProductMinimalDTO ToProductMinimalDTO(this Product entity)
+        {
+            return new ProductMinimalDTO
+            {
+                Id = entity.Id,
+                ManufacturerName = entity.ManufacturerName,
+                OrderNumber = entity.OrderNumber,
+                ProductName = entity.ProductName,
+                ShortDescription = entity.ShortDescription,
+                LongDescription = entity.LongDescription,
+
+                ImageLowQualityURL = entity.ImageLowQualityURL,
+
+                InStock = entity.InStock,
+                Price = entity.Price,
+                PriceCurrency = entity.PriceCurrency
+            };
+        }
+
+    }
+}
