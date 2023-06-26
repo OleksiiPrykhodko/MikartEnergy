667f681d764561e70fb0ebf3eb13cf034e850327
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-06-26 19:34:56 +0300

    Code Refactoring of backend ProductDTO.

--- backend/MikartEnergy.Common/DTO/Product/ProductDTO.cs
+++ backend/MikartEnergy.Common/DTO/Product/ProductDTO.cs
@@ -11,24 +11,24 @@ namespace MikartEnergy.Common.DTO.Product
 {
     public class ProductDTO : ResponseStatusDTO
     {
-        public string Id { get; set; }
-        public string ManufacturerName { get; set; }
-        public string OrderNumber { get; set; }
-        public string ProductName { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { set; get; }
+        public string Id { get; set; } = string.Empty;
+        public string ManufacturerName { get; set; } = string.Empty;
+        public string OrderNumber { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public string ShortDescription { get; set; } = string.Empty;
+        public string LongDescription { set; get; } = string.Empty;
         public IEnumerable<KeyValuePair<string, IEnumerable<string>>> TechnicalData { get; set; }
 
-        public string ImageLowQualityURL { get; set; }
-        public string ImageHighQualityURL { get; set; }
-        public string PdfWith3dURL { get; set; }
+        public string ImageLowQualityURL { get; set; } = string.Empty;
+        public string ImageHighQualityURL { get; set; } = string.Empty;
+        public string PdfWith3dURL { get; set; } = string.Empty;
 
-        public string LinkToProductPage { get; set; }
-        public string LinkToManuals { get; set; }
-        public string LinkToFAQ { get; set; }
-        public string LinkToTechnicalData { get; set; }
-        public string LinkToApplicationExample { get; set; }
-        public string LinkToVideo { get; set; }
+        public string LinkToProductPage { get; set; } = string.Empty;
+        public string LinkToManuals { get; set; } = string.Empty;
+        public string LinkToFAQ { get; set; } = string.Empty;
+        public string LinkToTechnicalData { get; set; } = string.Empty;
+        public string LinkToApplicationExample { get; set; } = string.Empty;
+        public string LinkToVideo { get; set; } = string.Empty;
 
         public IEnumerable<string> RelatedProductIDs { get; set; }
 
@@ -37,7 +37,7 @@ namespace MikartEnergy.Common.DTO.Product
         public int OrderQuantityMultiplier { get; set; } = 1;
         public bool InStock { get; set; } = true;
         public decimal Price { get; set; } = 0.00m;
-        public string PriceCurrency { get; set; }
+        public string PriceCurrency { get; set; } = string.Empty;
 
     }
 }
