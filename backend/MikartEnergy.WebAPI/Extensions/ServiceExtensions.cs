ff3df0f51a710f3a13ccfd0ff1af62ceeb92159e
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-04 18:15:57 +0300

    I modified the RegisterCustomServices() method to register a service EtimProductsXmlReader.

--- backend/MikartEnergy.WebAPI/Extensions/ServiceExtensions.cs
+++ backend/MikartEnergy.WebAPI/Extensions/ServiceExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using FluentValidation;
 using MikartEnergy.BLL.Services;
 using MikartEnergy.Common.DTO.CallbackRequest;
+using MikartEnergy.DAL.Context.ETIM_files_reading;
 using MikartEnergy.WebAPI.Validators;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace MikartEnergy.WebAPI.Extensions
 {
@@ -11,8 +14,15 @@ namespace MikartEnergy.WebAPI.Extensions
         /// Extension method for registering business logic services.
         /// </summary>
         /// <param name="services"></param>
-        public static void RegisterCustomServices(this IServiceCollection services)
+        public static void RegisterCustomServices(this IServiceCollection services, WebApplicationBuilder builder)
         {
+            // ETIM file reading service registration.
+            var etimFilePath = builder.Configuration["EtimXmlFilePath"];
+            var pathToAssembly = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+            services.AddSingleton<IEtimProductsFileReader, EtimProductsXmlReader>(
+                x => new EtimProductsXmlReader(pathToAssembly + "\\" + etimFilePath));
+
+            services.AddSingleton<ProductService>();
             services.AddScoped<CallbackRequestService>();
         }
 
