d7bff7c274e2e3a94e6e193720510525343b943c
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-09 13:59:21 +0300

    I changed the CallbackRequestController, CallbackRequestService and ProductsController, ProductsService to work with the new ResultModel class.

--- backend/MikartEnergy.BLL/Services/ProductService.cs
+++ backend/MikartEnergy.BLL/Services/ProductService.cs
@@ -4,6 +4,7 @@ using MikartEnergy.Common.DTO.CallbackRequest;
 using MikartEnergy.Common.DTO.Pagination;
 using MikartEnergy.Common.DTO.Product;
 using MikartEnergy.Common.Enums;
+using MikartEnergy.Common.Models.Result;
 using MikartEnergy.DAL.Context;
 using MikartEnergy.DAL.Context.ETIM_files_reading;
 using MikartEnergy.DAL.Entities;
@@ -23,64 +24,68 @@ namespace MikartEnergy.BLL.Services
         private List<Product> _productsList;
 
         public ProductService(IEtimProductsFileReader etimProductsFileReader)
-        { 
+        {
             _etimProductsFileReader = etimProductsFileReader;
             _productsList = _etimProductsFileReader.GetProducts().ToList();
         }
 
-        public async Task<PaginationResponseDTO<ProductDTO>> GetAllProductsAsync(PaginationRequestDTO request)
+        public async Task<ResultModel<PaginationResponseDTO<ProductDTO>>> GetAllProductsAsync(PaginationRequestDTO request)
         {
             var skipeAmount = (request.PageIndex - 1) * request.PageSize;
 
-            return await Task.Run(() => new PaginationResponseDTO<ProductDTO>()
+            return await Task.Run(() =>
+            new ResultModel<PaginationResponseDTO<ProductDTO>>(new PaginationResponseDTO<ProductDTO>()
             {
                 Items = _productsList.Skip(skipeAmount).Take(request.PageSize).Select(p => p.ToProductDTO()),
                 TotalItemsNumber = _productsList.Count()
-            });
+            }));
         }
 
-        public async Task<ProductDTO> GetProductByIdAsync(string id)
+        public async Task<ResultModel<ProductDTO>> GetProductByIdAsync(string id)
         {
             var product = _productsList.Find(p => p.Id == id);
 
             if (product is not null)
             {
-                return await Task.Run(() => product.ToProductDTO());
+                return await Task.Run(() => new ResultModel<ProductDTO>(product.ToProductDTO()));
             }
 
             return await Task.Run(() =>
             {
                 var productDTO = new ProductDTO() { Id = id };
-                productDTO.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
-                return productDTO;
+                var result = new ResultModel<ProductDTO>(productDTO);
+                result.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
+                return result;
             });
         }
 
-        public async Task<PaginationResponseDTO<ProductMinimalDTO>> GetAllProductsMinamalAsync(PaginationRequestDTO request)
+        public async Task<ResultModel<PaginationResponseDTO<ProductMinimalDTO>>> GetAllProductsMinamalAsync(PaginationRequestDTO request)
         {
             var skipeAmount = (request.PageIndex - 1) * request.PageSize;
 
-            return await Task.Run(() => new PaginationResponseDTO<ProductMinimalDTO>()
+            return await Task.Run(() => 
+            new ResultModel<PaginationResponseDTO<ProductMinimalDTO>>(new PaginationResponseDTO<ProductMinimalDTO>()
             {
                 Items = _productsList.Skip(skipeAmount).Take(request.PageSize).Select(p => p.ToProductMinimalDTO()),
                 TotalItemsNumber = _productsList.Count()
-            });
+            }));
         }
 
-        public async Task<ProductMinimalDTO> GetProductMinamalByIdAsync(string id)
+        public async Task<ResultModel<ProductMinimalDTO>> GetProductMinamalByIdAsync(string id)
         {
             var product = _productsList.Find(p => p.Id == id);
 
             if (product is not null)
             {
-                return await Task.Run(() => product.ToProductMinimalDTO());
+                return await Task.Run(() => new ResultModel<ProductMinimalDTO>(product.ToProductMinimalDTO()));
             }
 
             return await Task.Run(() =>
             {
                 var productDTO = new ProductMinimalDTO() { Id = id };
-                productDTO.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
-                return productDTO;
+                var result = new ResultModel<ProductMinimalDTO>(productDTO);
+                result.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
+                return result;
             });
         }
 
--- backend/MikartEnergy.WebAPI/Controllers/ProductsController.cs
+++ backend/MikartEnergy.WebAPI/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using MikartEnergy.BLL.Services;
 using MikartEnergy.Common.DTO.CallbackRequest;
 using MikartEnergy.Common.DTO.Pagination;
 using MikartEnergy.Common.DTO.Product;
+using MikartEnergy.Common.Models.Result;
 using System.Security.Cryptography.X509Certificates;
 
 namespace MikartEnergy.WebAPI.Controllers
@@ -23,28 +24,28 @@ namespace MikartEnergy.WebAPI.Controllers
 
         [HttpGet]
         [AllowAnonymous]
-        public async Task<ActionResult<PaginationResponseDTO<ProductDTO>>> Get([FromQuery] PaginationRequestDTO request)
+        public async Task<ActionResult<ResultModel<PaginationResponseDTO<ProductDTO>>>> Get([FromQuery] PaginationRequestDTO request)
         {
             return Ok(await _productsService.GetAllProductsAsync(request));
         }
 
         [HttpGet("{id}")]
         [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get(string id)
+        public async Task<ActionResult<ResultModel<IEnumerable<ProductDTO>>>> Get(string id)
         {
             return Ok(await _productsService.GetProductByIdAsync(id));
         }
 
         [HttpGet("minimal")]
         [AllowAnonymous]
-        public async Task<ActionResult<PaginationResponseDTO<ProductMinimalDTO>>> GetProductsMinamal([FromQuery] PaginationRequestDTO request)
+        public async Task<ActionResult<ResultModel<PaginationResponseDTO<ProductMinimalDTO>>>> GetProductsMinamal([FromQuery] PaginationRequestDTO request)
         {
             return Ok(await _productsService.GetAllProductsMinamalAsync(request));
         }
 
         [HttpGet("minimal/{id}")]
         [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<ProductMinimalDTO>>> GetProductMinamal(string id)
+        public async Task<ActionResult<ResultModel<IEnumerable<ProductMinimalDTO>>>> GetProductMinamal(string id)
         {
             return Ok(await _productsService.GetProductMinamalByIdAsync(id));
         }
