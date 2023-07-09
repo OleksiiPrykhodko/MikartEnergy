85867eee10d0dbc458317fa82502e0be89bb2f8b
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-09 17:42:16 +0300

    I've removed all logic from the PaginationRequestDTO.

--- backend/MikartEnergy.Common/DTO/Pagination/PaginationRequestDTO.cs
+++ backend/MikartEnergy.Common/DTO/Pagination/PaginationRequestDTO.cs
@@ -9,19 +9,8 @@ namespace MikartEnergy.Common.DTO.Pagination
 {
     public class PaginationRequestDTO
     {
-        private int _pageSize = 50;
-        private int _pageIndex = 1;
-
-        public int PageIndex
-        {
-            get { return _pageIndex; }
-            set { _pageIndex = value < 1 ? 1 : value; }
-        }
-        public int PageSize
-        {
-            get { return _pageSize; }
-            set { _pageSize = value > _pageSize || value < 1 ? _pageSize : value; }
-        }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
     }
 
 }
