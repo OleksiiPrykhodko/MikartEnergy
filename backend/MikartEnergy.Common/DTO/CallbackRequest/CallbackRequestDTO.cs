2b94d5b88271577257f202a89c8374bec98bacc8
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-09 14:11:18 +0300

    I've added = string.Empty; to all string property of DTOs.

--- backend/MikartEnergy.Common/DTO/CallbackRequest/CallbackRequestDTO.cs
+++ backend/MikartEnergy.Common/DTO/CallbackRequest/CallbackRequestDTO.cs
@@ -8,12 +8,12 @@
         public bool InWork { get; set; }
         public bool IsDeleted { get; set; }
 
-        public string AuthorFirstName { get; set; }
-        public string AuthorLastName { get; set; }
-        public string AuthorEmail { get; set; }
-        public string AuthorPhone { get; set; }
-        public string Message { get; set; }
-        public string IntrerestedIn { get; set; }
+        public string AuthorFirstName { get; set; } = string.Empty;
+        public string AuthorLastName { get; set; } = string.Empty;
+        public string AuthorEmail { get; set; } = string.Empty;
+        public string AuthorPhone { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string IntrerestedIn { get; set; } = string.Empty;
         public int Budget { get; set; }
     }
 }
--- backend/MikartEnergy.Common/DTO/CallbackRequest/NewCallbackRequestDTO.cs
+++ backend/MikartEnergy.Common/DTO/CallbackRequest/NewCallbackRequestDTO.cs
@@ -2,12 +2,12 @@
 {
     public class NewCallbackRequestDTO
     {
-        public string AuthorFirstName { get; set; }
-        public string AuthorLastName { get; set; }
-        public string AuthorEmail { get; set; }
-        public string AuthorPhone { get; set; }
-        public string Message { get; set; }
-        public string IntrerestedIn { get; set; }
+        public string AuthorFirstName { get; set; } = string.Empty;
+        public string AuthorLastName { get; set; } = string.Empty;
+        public string AuthorEmail { get; set; } = string.Empty;
+        public string AuthorPhone { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string IntrerestedIn { get; set; } = string.Empty;
         public int Budget { get; set; }
     }
 }
