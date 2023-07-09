9c6b41b7e878e7c387a82016f9d4ee15fcc9911b
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-09 13:56:27 +0300

    I added a new helper ResultModel to send information to the frontend about query errors. Such as invalid DTO fields or no entity in the database when trying to change or delete an entity. The old ResponceStatusDTO base class will be removed due to its non-convenience.

---
+++ backend/MikartEnergy.Common/Models/Result/ResultModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.Common.Models.Result
+{
+    public class ResultModel<T> where T : class
+    {
+        public ResultModel(T item)
+        {
+            DTO = item;
+        }
+        public T DTO { get; }
+        public bool Successful { get; private set; } = true;
+        public List<KeyValuePair<string, string>> Errors { get; } =
+            new List<KeyValuePair<string, string>>();
+
+        public void AddErrorToDTO(string error, string message)
+        {
+            Successful = false;
+            Errors.Add(new KeyValuePair<string, string>(error, message));
+        }
+
+        public void AddErrorToDTO(IEnumerable<KeyValuePair<string, string>> errors)
+        {
+            Successful = false;
+            Errors.AddRange(errors);
+        }
+    }
+}
