a3834ac63254980d215dfce3426622ff5886e2b8
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-13 12:27:45 +0300

    I've created component product-minimal-slim for view of product card with short description and link.

---
+++ frontend/src/app/components/shop-components/product-minimal-slim/product-minimal-slim.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-minimal-slim',
+  templateUrl: './product-minimal-slim.component.html',
+  styleUrls: ['./product-minimal-slim.component.scss']
+})
+export class ProductMinimalSlimComponent {
+
+}
