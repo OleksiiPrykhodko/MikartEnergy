3664fe20bb9aae580a96d54effd6dd4209abeba5
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-13 19:27:00 +0300

    I've added layout and logic to product-page.component.

--- frontend/src/app/components/shop-components/product-minimal-slim/product-minimal-slim.component.ts
+++ frontend/src/app/components/shop-components/product-minimal-slim/product-minimal-slim.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductMinimal } from 'src/app/models/product/prodact-minimal';
 
 @Component({
   selector: 'app-product-minimal-slim',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-minimal-slim.component.scss']
 })
 export class ProductMinimalSlimComponent {
+  @Input() _product: ProductMinimal;
+
+
+
+  public getLinkToProductPage(): string {
+    return "";
+  }
+  public getProductName(): string{
+    return this._product.productName;
+  }
+  public getImageLowQualityURL(): string {
+    if (this._product.imageLowQualityURL) {
+      return this._product.imageLowQualityURL;
+    }
+    return ""; //TODO: Add some no found img pictire. 
+  }
+  public getOrderNumber(): string{
+    return this._product.orderNumber;
+  }
+  public getPriceAmount(): number {
+    return this._product.price;
+  }
+  public getPriceCurrency(): string {
+    return this._product.priceCurrency;
+  }
 
 }
