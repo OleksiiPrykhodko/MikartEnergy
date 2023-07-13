03573cbce5fdec6fdcddb48422ddd3c016482fb9
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-13 12:29:27 +0300

    I've created component product-minimal-wide for viewing of product card with short description and link.

---
+++ frontend/src/app/components/shop-components/product-minimal-wide/product-minimal-wide.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-minimal-wide',
+  templateUrl: './product-minimal-wide.component.html',
+  styleUrls: ['./product-minimal-wide.component.scss']
+})
+export class ProductMinimalWideComponent {
+
+}
