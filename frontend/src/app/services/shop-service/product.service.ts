2b8ee71d0d12c6cdd1abe841aa3e8f1297fd882b
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-16 13:04:15 +0300

    I've modified product.service.ts for using RequestResult<T>.

--- frontend/src/app/services/shop-service/product.service.ts
+++ frontend/src/app/services/shop-service/product.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpInternalService } from '../http-internal.service';
 import { Product } from 'src/app/models/product/product';
 import { ProductMinimal } from 'src/app/models/product/prodact-minimal';
+import { RequestResult } from 'src/app/models/common/request-result';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +16,12 @@ export class ProductService {
 
   constructor(private httpService: HttpInternalService) { }
 
-  public getProductById(productID: string) {
-    return this.httpService.getFullRequest<Product>(`${this.routePrefix}/${productID}`);
+  public getProductById(productID: string): Observable<HttpResponse<RequestResult<Product>>> {
+    return this.httpService.getFullRequest<RequestResult<Product>>(`${this.routePrefix}/${productID}`);
   }
 
-  public getProductMinimalById(productID: string) {
-    return this.httpService.getFullRequest<ProductMinimal>(`${this.routeMinimalPrefix}/${productID}`);
+  public getProductMinimalById(productID: string): Observable<HttpResponse<RequestResult<ProductMinimal>>> {
+    return this.httpService.getFullRequest<RequestResult<ProductMinimal>>(`${this.routeMinimalPrefix}/${productID}`);
   }
 
 }
