a3abaf544dea8fffd51ae8b84ad73dbbaf4c531d
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-16 13:51:44 +0300

    I've added main logic to safe-url.pipe.ts.

--- frontend/src/app/pipes/safe-url/safe-url.pipe.ts
+++ frontend/src/app/pipes/safe-url/safe-url.pipe.ts
@@ -1,12 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform} from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
 @Pipe({
   name: 'safeUrl'
 })
 export class SafeUrlPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  constructor(private sanitizer: DomSanitizer) { }
+
+  transform(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
 }
