45fd9b5a24efde8040d6eaa5b924fe8553c5de13
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-31 19:49:50 +0300

    I use new srvice in ConfiguratorController.

--- backend/MikartEnergy.WebAPI/Controllers/ConfiguratorController.cs
+++ backend/MikartEnergy.WebAPI/Controllers/ConfiguratorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MikartEnergy.BLL.Services;
 using MikartEnergy.Common.DTO.Configurator;
 using MikartEnergy.WebAPI.ModelBinders;
 
@@ -10,15 +11,22 @@ namespace MikartEnergy.WebAPI.Controllers
     [ApiController]
     public class ConfiguratorController : ControllerBase
     {
+        private readonly ConfiguratorResultService _configuratorService;
+
+        public ConfiguratorController(ConfiguratorResultService configuratorService)
+        {
+            _configuratorService = configuratorService;
+        }
+
         // POST api/<ConfiguratorController>
         [HttpPost]
         [Consumes("multipart/form-data")] // Do we need this ? 
         [AllowAnonymous]
         public IActionResult Post([ModelBinder(typeof(TiaStOrderModelBinder))] TiaStResultDTO[] result)
         {
+            var id = _configuratorService.CreateConfiguratorResult(result);
 
-
-            var redirect = new RedirectResult($"http://localhost:4200/shop/configurator/", true); // Add ID 
+            var redirect = new RedirectResult($"http://localhost:4200/shop/configurator/{id}", true); // Add ID 
             return redirect;
         }
 
