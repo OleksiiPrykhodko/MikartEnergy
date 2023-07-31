68485263e935fca2bf02a61dc490da3f8a1f62ef
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-31 17:30:37 +0300

    I've created ConfiguratorResultService.

---
+++ backend/MikartEnergy.BLL/Services/ConfiguratorResultService.cs
@@ -0,0 +1,39 @@
+﻿using MikartEnergy.Common.DTO.Configurator;
+using MikartEnergy.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.BLL.Services
+{
+    public class ConfiguratorResultService
+    {
+        List<TiaStConfiguratorResult> ConfiguratorResults = new List<TiaStConfiguratorResult>();
+
+        public ConfiguratorResultService() 
+        { 
+        
+        }
+
+        public Guid CreateConfiguratorResult(TiaStResultDTO[] tiaStResult)
+        {
+
+
+            var resultGuid = Guid.NewGuid();
+            while(ConfiguratorResults.Any(r => r.Id.Equals(resultGuid)))
+            {
+                resultGuid = Guid.NewGuid();
+            }
+            var createdResult = new TiaStConfiguratorResult()
+            {
+                Id = resultGuid
+
+            };
+
+            return resultGuid;
+        }
+
+    }
+}
