e5b5fa3bffd9ccbc14cc4a7255aff795544b12e5
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-07-31 19:44:43 +0300

    I've added logic to ConfiguratorResultService for saving new TIA ST configurator results in memory.

--- backend/MikartEnergy.BLL/Services/ConfiguratorResultService.cs
+++ backend/MikartEnergy.BLL/Services/ConfiguratorResultService.cs
@@ -1,4 +1,5 @@
 ﻿using MikartEnergy.Common.DTO.Configurator;
+using MikartEnergy.DAL.Context.ETIM_files_reading;
 using MikartEnergy.DAL.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,29 +11,47 @@ namespace MikartEnergy.BLL.Services
 {
     public class ConfiguratorResultService
     {
-        List<TiaStConfiguratorResult> ConfiguratorResults = new List<TiaStConfiguratorResult>();
+        private List<TiaStConfiguratorResult> ConfiguratorResults = new List<TiaStConfiguratorResult>();
+        private readonly IEtimProductsFileReader _etimProductsFileReader;
+        private readonly List<Product> _productsList;
 
-        public ConfiguratorResultService() 
-        { 
-        
+        public ConfiguratorResultService(IEtimProductsFileReader etimProductsFileReader) 
+        {
+            _etimProductsFileReader = etimProductsFileReader;
+            _productsList = _etimProductsFileReader.GetProducts().ToList();
         }
 
         public Guid CreateConfiguratorResult(TiaStResultDTO[] tiaStResult)
         {
+            var createdResult = new TiaStConfiguratorResult();
 
+            var existingProducts = new List<KeyValuePair<string, int>>();
+            var notExistingProducts = new List<KeyValuePair<string, int>>();
+            foreach (var result in tiaStResult)
+            {
+                var pair = new KeyValuePair<string, int>(result.MANUFACTURER_PID, int.Parse(result.QUANTITY));
+                if (_productsList.Any(p => p.OrderNumber.ToUpper() == result.MANUFACTURER_PID.ToUpper()))
+                {
+                    existingProducts.Add(pair);
+                }
+                else
+                {
+                    notExistingProducts.Add(pair);
+                }
+            }
 
+            // Create unique Id.
             var resultGuid = Guid.NewGuid();
-            while(ConfiguratorResults.Any(r => r.Id.Equals(resultGuid)))
+            while (ConfiguratorResults.Any(r => r.Id.Equals(resultGuid)))
             {
                 resultGuid = Guid.NewGuid();
             }
-            var createdResult = new TiaStConfiguratorResult()
-            {
-                Id = resultGuid
+            
+            createdResult.Id = resultGuid;
+            createdResult.ExistingProducts = existingProducts;
+            createdResult.NotExistingProducts = notExistingProducts;
 
-            };
-
-            return resultGuid;
+            return createdResult.Id;
         }
 
     }
