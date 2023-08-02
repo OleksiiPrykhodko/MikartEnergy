cd59d216a4bc34884693837edd3e1da90826007a
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-08-02 18:03:02 +0300

    I've added logic to configurator-result-page component.

--- frontend/src/app/pages/shop-pages/configurator-result-page/configurator-result-page.component.ts
+++ frontend/src/app/pages/shop-pages/configurator-result-page/configurator-result-page.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { keyValuePair } from 'src/app/models/common/keyValuePair';
+import { TiaStProductsOrder } from 'src/app/models/configurator/tiast-products-order';
+import { ProductMinimal } from 'src/app/models/product/prodact-minimal';
+import { ConfiguratorService } from 'src/app/services/shop-service/configurator.service';
 
 @Component({
   selector: 'app-configurator-result-page',
@@ -6,5 +12,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./configurator-result-page.component.scss']
 })
 export class ConfiguratorResultPageComponent {
+
+  private _infoIsLoading: boolean = true;
+  private _subscriptionToRoutParamChange: Subscription;
+  private _configurationResultIdFromRoute: string;
+  private _configuratorSubscription: Subscription;
+  private _tiaStProductsOrder: TiaStProductsOrder;
+
+  constructor( 
+    private _activateRoute: ActivatedRoute,
+    private _configuratorService: ConfiguratorService,
+    private _router: Router) 
+  {
+    this._subscriptionToRoutParamChange =
+      _activateRoute.params.subscribe(params => this._configurationResultIdFromRoute = params["resultID"]);
+  }
+
+  ngOnInit(){
+    this._configuratorSubscription = this._configuratorService.getConfigurationResultById(this._configurationResultIdFromRoute)
+    .subscribe(result =>
+      {
+        if(result.url !== null && result.body !== null && result.ok){
+          if(result.body?.successful){
+            // All OK
+            this._tiaStProductsOrder = result.body?.dto;
+            this._infoIsLoading = false;
+          }
+          else{
+            // Result model with successful = false
+            // If the error is Id not found (key = "NotFound"), redirect to page "page not found".
+            if(result.body?.errors.some(e => e.key === "NotFound")){
+              this._router.navigate(['404']);
+            }
+            else{
+              result.body?.errors.forEach(error => console.log(`Error: ${error.key}. Description: ${error.key}.`));
+            }
+          }
+        }
+        else{
+          if(result.url == null){
+            console.log(`The server is not available.`);
+          }
+          else{
+            if(result.body == null){
+              console.log(`Server response error. No required response body.`);
+            }
+            console.log(`Something go wrong. HTTP response code: ${result.status}`);
+          }
+        }
+      })
+  }
+
+  ngOnDestroy() {
+    this._subscriptionToRoutParamChange.unsubscribe();
+    this._configuratorSubscription.unsubscribe();
+  }
+
+  public getExistingProducts(): ProductMinimal[]{
+    return this._tiaStProductsOrder?.existingInDbProducts.map(p => p.key) ?? [];
+  }
+
+  public getUnExistingProducts(): keyValuePair<string, number>[]{
+    return this._tiaStProductsOrder?.notExistingInDbProducts ?? [];
+  }
 
 }
