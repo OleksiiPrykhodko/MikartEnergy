950bf3e2cdaedba91b05ab1db5e6e8509899a185
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-08-03 18:23:02 +0300

    I've modified layout and logic of wide-product-minimals.component.

--- frontend/src/app/components/shop-components/wide-product-minimals/wide-product-minimals.component.ts
+++ frontend/src/app/components/shop-components/wide-product-minimals/wide-product-minimals.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { keyValuePair } from 'src/app/models/common/keyValuePair';
+import { ProductMinimal } from 'src/app/models/product/prodact-minimal';
 
 @Component({
   selector: 'app-wide-product-minimals',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./wide-product-minimals.component.scss']
 })
 export class WideProductMinimalsComponent {
+  @Input() _products: keyValuePair<ProductMinimal, number>[];
+  @Input() _title: string;
 
+  ngOnChanges(){
+    this._products.forEach(pair => 
+      {
+        // If imageLowQualityURL is empty then set special img.
+        if(!pair.key.imageLowQualityURL){
+          pair.key.imageLowQualityURL = "assets/images/ImgNotFound.svg";
+        }
+      });
+  }
+
+  public getProductMinimals(): keyValuePair<ProductMinimal, number>[] {
+    return this._products;
+  }
+
+  public checkLoaded(): boolean{
+    return this._products.length > 0;
+  }
+
+  public setLinkToProductPage(productId: string | null | undefined): string{
+    if(productId){
+      return `shop/products/${productId}`;
+    }
+    return "404";
+  }
 }
