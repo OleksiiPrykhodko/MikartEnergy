d0f7a83543a91173553319fedec5e225d02e3c20
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-09-14 10:36:45 +0300

    I've created an entity TechnicalValue to store technical values in a database.

---
+++ backend/MikartEnergy.DAL/Entities/TechnicalValue.cs
@@ -0,0 +1,17 @@
+﻿using MikartEnergy.DAL.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.DAL.Entities
+{
+    public class TechnicalValue : BaseEntity
+    {
+        public string Description { get; set; } = string.Empty;
+        public string EtimCode { get; set; } = string.Empty;
+        public bool EtimDeprecated { get; set; } = false;
+        public List<TechnicalData> TechnicalData { get; set; } = new();
+    }
+}
