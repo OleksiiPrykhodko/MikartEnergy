62788b4daee2da2870c65d73bad501f7c3c05916
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-09-14 12:27:41 +0300

    I created an entity ProductOrderQuantity to store quantity of products in some order.

---
+++ backend/MikartEnergy.DAL/Entities/ProductOrderQuantity.cs
@@ -0,0 +1,19 @@
+﻿using MikartEnergy.DAL.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.DAL.Entities
+{
+    public class ProductOrderQuantity : BaseEntity
+    {
+        public Guid TiaStConfiguratorResultId { get; set; }
+        public TiaStConfiguratorResult TiaStConfiguratorResult { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
