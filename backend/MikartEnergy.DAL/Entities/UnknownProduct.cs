9a6926942633b1fc65bd49d9e6557b269eb1401f
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-09-14 12:28:56 +0300

    I've created an entity UnknownProduct to store data about unknown products from order. Unknown products can be received from the TIA ST configurator.

---
+++ backend/MikartEnergy.DAL/Entities/UnknownProduct.cs
@@ -0,0 +1,18 @@
+﻿using MikartEnergy.DAL.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.DAL.Entities
+{
+    public class UnknownProduct : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Guid TiaStConfiguratorResultId { get; set; }
+        public TiaStConfiguratorResult TiaStConfiguratorResult { get; set; }
+        public int Quantity { get; set; }
+    }
+}
