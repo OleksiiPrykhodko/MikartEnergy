d26333e4ae5ba0089a23c421127b75b1ba3f4294
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-09-15 11:11:29 +0300

    I'he added to the ProductMapper two new methods ToProductDTO and ToProductMinimalDTO for mapping Product to ProductDTO and Product to ProductMinimalDTO.

--- backend/MikartEnergy.BLL/Mapping/ProductMapper.cs
+++ backend/MikartEnergy.BLL/Mapping/ProductMapper.cs
@@ -11,7 +11,7 @@ namespace MikartEnergy.BLL.Mapping
 {
     public static class ProductMapper
     {
-        public static ProductDTO ToProductDTO(this Product entity)
+        public static ProductDTO ToProductDTO(this EtimProduct entity)
         {
             return new ProductDTO
             {
@@ -40,6 +40,65 @@ namespace MikartEnergy.BLL.Mapping
                 InStock = entity.InStock,
                 Price = entity.Price,
                 PriceCurrency = entity.PriceCurrency,
+            };
+        }
+
+        public static ProductMinimalDTO ToProductMinimalDTO(this EtimProduct entity)
+        {
+            return new ProductMinimalDTO
+            {
+                SupplierPID = entity.SupplierPID,
+                ManufacturerName = entity.ManufacturerName,
+                OrderNumber = entity.OrderNumber,
+                ProductName = entity.ProductName,
+                ShortDescription = entity.ShortDescription,
+                LongDescription = entity.LongDescription,
+
+                ImageLowQualityURL = entity.ImageLowQualityURL,
+
+                InStock = entity.InStock,
+                Price = entity.Price,
+                PriceCurrency = entity.PriceCurrency
+            };
+        }
+
+        public static ProductDTO ToProductDTO(this Product entity)
+        {
+            return new ProductDTO
+            {
+                SupplierPID = entity.SupplierPID,
+                ManufacturerName = entity.ManufacturerName,
+                OrderNumber = entity.OrderNumber,
+                ProductName = entity.ProductName,
+                ShortDescription = entity.ShortDescription,
+                LongDescription = entity.LongDescription,
+                TechnicalData = entity.TechnicalData.Select(td =>
+                {
+                    var technicalFeature = td.TechnicalFeature.Description;
+                    var technicalValues = td.TechnicalValues.Select(tv => tv.Description);
+                    var dataLine = new KeyValuePair<string, IEnumerable<string>>(technicalFeature, technicalValues);
+                    return dataLine;
+                }),
+
+                ImageLowQualityURL = entity.ImageLowQualityURL,
+                ImageHighQualityURL = entity.ImageHighQualityURL,
+                PdfWith3dURL = entity.PdfWith3dURL,
+
+                LinkToProductPage = entity.LinkToProductPage,
+                LinkToManuals = entity.LinkToManuals,
+                LinkToFAQ = entity.LinkToFAQ,
+                LinkToTechnicalData = entity.LinkToTechnicalData,
+                LinkToApplicationExample = entity.LinkToApplicationExample,
+                LinkToVideo = entity.LinkToVideo,
+
+                MinimalOrderQuantity = entity.MinimalOrderQuantity,
+                MaximalOrderQuantity = entity.MaximalOrderQuantity,
+                OrderQuantityMultiplier = entity.OrderQuantityMultiplier,
+                InStock = entity.InStock,
+                Price = entity.Price,
+                PriceCurrency = entity.PriceCurrency,
+
+                RelatedProducts = entity.RelatedProducts.Select(rp => rp.ToProductMinimalDTO())
             };
         }
 
