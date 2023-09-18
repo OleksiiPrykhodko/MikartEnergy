491cc3c2e07305490ec4ff33aa3eb89cf7926feb
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-09-18 10:19:44 +0300

    I've fully rebuilt ProductService to use MicartContext in it.

--- backend/MikartEnergy.BLL/Services/ProductService.cs
+++ backend/MikartEnergy.BLL/Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using MikartEnergy.BLL.Mapping;
+﻿using Microsoft.EntityFrameworkCore;
+using MikartEnergy.BLL.Mapping;
 using MikartEnergy.BLL.Services.Abstract;
 using MikartEnergy.Common.DTO.CallbackRequest;
 using MikartEnergy.Common.DTO.Pagination;
@@ -20,78 +21,87 @@ namespace MikartEnergy.BLL.Services
 {
     public class ProductService : BaseService
     {
-        private List<Product> _productsList;
         private readonly MikartContext _context;
 
-        public ProductService(IEtimProductsFileReader etimProductsFileReader, MikartContext context): base()
+        public ProductService(MikartContext context) : base()
         {
-            _productsList = etimProductsFileReader.GetProducts().ToList();
-
             _context = context;
         }
 
         public async Task<ResultModel<PaginationResponseDTO<ProductDTO>>> GetAllProductsAsync(PaginationRequestDTO request)
         {
-            return await Task.Run(() =>
-            new ResultModel<PaginationResponseDTO<ProductDTO>>(new PaginationResponseDTO<ProductDTO>()
+            var products = _context.Products
+                .Include(p => p.Keywords)
+                .Include(p => p.RelatedProducts)
+                .Include(p => p.TechnicalData)
+                .ThenInclude(td => td.TechnicalFeature)
+                .Include(p => p.TechnicalData)
+                .ThenInclude(t => t.TechnicalValues);
+
+            var paginationResponseDTO = new PaginationResponseDTO<ProductDTO>()
             {
-                Items = _productsList.Skip(base.GetSkipAmount(request)).Take(request.PageSize).Select(p => p.ToProductDTO()),
-                TotalItemsNumber = _productsList.Count()
-            }));
+                Items = products.Skip(base.GetSkipAmount(request)).Take(request.PageSize).Select(p => p.ToProductDTO()),
+                TotalItemsNumber = products.Count()
+            };
+
+            var resultModel = new ResultModel<PaginationResponseDTO<ProductDTO>>(paginationResponseDTO);
+            return resultModel;
         }
 
         public async Task<ResultModel<ProductDTO>> GetProductByIdAsync(string id)
         {
-            var idUpper = id.ToUpper();
-            var product = _productsList.Find(p => p.SupplierPID == idUpper);
+            var idUppercase = id.ToUpper();
+            var product = await _context.Products
+                .Include(p => p.Keywords)
+                .Include(p => p.RelatedProducts)
+                .Include(p => p.TechnicalData)
+                .ThenInclude(td => td.TechnicalFeature)
+                .Include(p => p.TechnicalData)
+                .ThenInclude(td => td.TechnicalValues)
+                .FirstOrDefaultAsync(p => p.SupplierPID == idUppercase);
 
             if (product is not null)
             {
-                var dto = product.ToProductDTO();
-                dto.RelatedProducts = _productsList
-                    .IntersectBy(product.RelatedProductIDs, p => p.SupplierPID)
-                    .Select(p => p.ToProductMinimalDTO());
-                return await Task.Run(() => new ResultModel<ProductDTO>(dto));
+                var productDTO = product.ToProductDTO();
+                var resultModel = new ResultModel<ProductDTO>(productDTO);
+                return resultModel;
             }
 
-            return await Task.Run(() =>
-            {
-                var productDTO = new ProductDTO() { SupplierPID = idUpper };
-                var result = new ResultModel<ProductDTO>(productDTO);
-                result.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
-                return result;
-            });
+            var productErrorDTO = new ProductDTO() { SupplierPID = idUppercase };
+            var result = new ResultModel<ProductDTO>(productErrorDTO);
+            result.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
+            return result;
         }
 
         public async Task<ResultModel<PaginationResponseDTO<ProductMinimalDTO>>> GetAllProductsMinamalAsync(PaginationRequestDTO request)
         {
-            return await Task.Run(() => 
-            new ResultModel<PaginationResponseDTO<ProductMinimalDTO>>(new PaginationResponseDTO<ProductMinimalDTO>()
+            var products = await _context.Products.ToListAsync();
+            var paginationResponseDTO = new PaginationResponseDTO<ProductMinimalDTO>()
             {
-                Items = _productsList.Skip(base.GetSkipAmount(request)).Take(request.PageSize).Select(p => p.ToProductMinimalDTO()),
-                TotalItemsNumber = _productsList.Count()
-            }));
+                Items = products.Skip(base.GetSkipAmount(request)).Take(request.PageSize).Select(p => p.ToProductMinimalDTO()),
+                TotalItemsNumber = products.Count()
+            };
+            var resultModel = new ResultModel<PaginationResponseDTO<ProductMinimalDTO>>(paginationResponseDTO);
+            return resultModel;
         }
 
         public async Task<ResultModel<ProductMinimalDTO>> GetProductMinamalByIdAsync(string id)
         {
-            var idUpper = id.ToUpper();
-            var product = _productsList.Find(p => p.SupplierPID == idUpper);
+            var idUppercase = id.ToUpper();
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.SupplierPID == idUppercase);
 
             if (product is not null)
             {
-                return await Task.Run(() => new ResultModel<ProductMinimalDTO>(product.ToProductMinimalDTO()));
+                var productDTO = product.ToProductMinimalDTO();
+                var resultModel = new ResultModel<ProductMinimalDTO>(productDTO);
+                return resultModel;
             }
 
-            return await Task.Run(() =>
-            {
-                var productDTO = new ProductMinimalDTO() { SupplierPID = idUpper };
-                var result = new ResultModel<ProductMinimalDTO>(productDTO);
-                result.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
-                return result;
-            });
+            var productErrorDTO = new ProductMinimalDTO() { SupplierPID = idUppercase };
+            var result = new ResultModel<ProductMinimalDTO>(productErrorDTO);
+            result.AddErrorToDTO(ResponseError.NotFound.ToString(), "Product was not found by ID.");
+            return result;
         }
-
 
 
     }
