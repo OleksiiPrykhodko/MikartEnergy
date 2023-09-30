686cba9bb4580eda6d0949e2ab2d8b9ee3e930e9
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-09-30 16:46:39 +0300

    I added logic to the GlobalExceptionHandlingMiddleware.

--- backend/MikartEnergy.WebAPI/Middlewares/GlobalExceptionHandlingMiddleware.cs
+++ backend/MikartEnergy.WebAPI/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -1,7 +1,47 @@
-﻿namespace MikartEnergy.WebAPI.Middlewares
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace MikartEnergy.WebAPI.Middlewares
 {
     public class GlobalExceptionHandlingMiddleware
     {
+        private readonly RequestDelegate _requestDelegate;
 
+        public GlobalExceptionHandlingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsynk(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            // Alternatively, error information can be added here. 
+            var problem = new ProblemDetails()
+            {
+                Status = (int)HttpStatusCode.InternalServerError,
+                Type = "Server Error",
+                Title = "Server Error",
+                Detail = "As internal server error has occurred"
+            };
+
+            string problemAsJson = JsonSerializer.Serialize(problem);
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            return context.Response.WriteAsync(problemAsJson);
+        }
     }
 }
