085f142e5d2db5a323434403b81b71d3e11e17ef
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-10-04 12:13:54 +0300

    I've just added to the GlobalExceptionHandlingMiddleware.cs logging of Exception and Request info.

--- backend/MikartEnergy.WebAPI/Middlewares/GlobalExceptionHandlingMiddleware.cs
+++ backend/MikartEnergy.WebAPI/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -7,41 +7,63 @@ namespace MikartEnergy.WebAPI.Middlewares
     public class GlobalExceptionHandlingMiddleware
     {
         private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger _logger;
 
-        public GlobalExceptionHandlingMiddleware(RequestDelegate requestDelegate)
+        private string _bodyContent = string.Empty;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate requestDelegate, 
+            ILogger<GlobalExceptionHandlingMiddleware> logger)
         {
             _requestDelegate = requestDelegate;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             try
             {
+                _bodyContent = await ReadRequestBody(context.Request);
                 await _requestDelegate(context);
             }
-            catch (Exception ex)
+            catch (Exception exception)
             {
-                await HandleExceptionAsync(context, ex);
+                var exceptionType = exception.GetType().ToString();
+                var dateTime = DateTime.Now;
+
+                _logger.LogError("[{@DateTime}] - EXCEPTION Handling Middleware - {@ExceptionType} - {@Exception} - {@RequestPath} - {@RequestHeaders} - {@RequestBody}", dateTime, exceptionType, exception, context.Request.Path.ToString(), context.Request.Headers, _bodyContent);
+
+                // Creating respons with specific information about error.
+                var problem = new ProblemDetails()
+                {
+                    // Alternatively, error information can be added here.
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Type = "Server Error",
+                    Title = "Server Error",
+                    Detail = "As internal server error has occurred"
+                };
+
+                string problemAsJson = JsonSerializer.Serialize(problem);
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync(problemAsJson);
             }
         }
 
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private static async Task<string> ReadRequestBody(HttpRequest request)
         {
-            // Alternatively, error information can be added here. 
-            var problem = new ProblemDetails()
-            {
-                Status = (int)HttpStatusCode.InternalServerError,
-                Type = "Server Error",
-                Title = "Server Error",
-                Detail = "As internal server error has occurred"
-            };
+            // Ensure the request's body can be read multiple times (for the next middlewares in the pipeline).
+            request.EnableBuffering();
+            request.Body.Position = 0;
+            using var streamReader = new StreamReader(request.Body, leaveOpen: true);
+            var requestBody = await streamReader.ReadToEndAsync();
 
-            string problemAsJson = JsonSerializer.Serialize(problem);
-
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            context.Response.ContentType = "application/json";
-
-            return context.Response.WriteAsync(problemAsJson);
+            // Reset the request's body stream position for next middleware in the pipeline.
+            request.Body.Position = 0;
+            requestBody = requestBody ?? "";
+            return requestBody;
         }
+
     }
 }
