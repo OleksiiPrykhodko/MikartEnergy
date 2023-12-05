76b8d84510568602c730a83aaa15e6321b0ca80e
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-12-05 21:34:31 +0200

    I've added all needed NuGet packages to the MikartEnergy.UnitTest and deleted unnecessary file UnitTest1.cs.

---
+++ backend/MikartEnergy.UnitTests/Systems/Services/TestCallbackRequestService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MikartEnergy.DAL.Context;
+using MikartEnergy.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.UnitTests.Systems.Services
+{
+    public class TestCallbackRequestService
+    {
+        [Fact]
+        public void GetAllCallbackRequestsAsync_GetNotDeletedCallbackRequests_ReturnOnlyNotDeleted()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+    }
+}
