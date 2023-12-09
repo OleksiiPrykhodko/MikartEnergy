4088cfa4e123589f68f3836c24a54e4c008f3eba
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-12-09 19:07:25 +0200

    I've added to the test project first test in my life.

--- backend/MikartEnergy.UnitTests/Systems/Services/TestCallbackRequestService.cs
+++ backend/MikartEnergy.UnitTests/Systems/Services/TestCallbackRequestService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using MikartEnergy.BLL.Services;
 using MikartEnergy.DAL.Context;
 using MikartEnergy.DAL.Entities;
@@ -13,12 +14,81 @@ namespace MikartEnergy.UnitTests.Systems.Services
     public class TestCallbackRequestService
     {
         [Fact]
-        public void GetAllCallbackRequestsAsync_GetNotDeletedCallbackRequests_ReturnOnlyNotDeleted()
+        public async void GetAllCallbackRequestsAsync_GetNotDeletedCallbackRequests_ReturnOnlyNotDeleted()
         {
             //Arrange
+            var databaseContext = CreateContext();
+
+            databaseContext.CallbackRequests.AddRange(
+                new CallbackRequest()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    IsDeleted = true,
+
+                    AuthorEmail = "DeletedEmail@email.com",
+                    AuthorFirstName = "Test",
+                    AuthorLastName = "Test",
+                    AuthorPhone = "Test",
+                    Budget = 100,
+                    Message = "I like tests!",
+                    IntrerestedIn = "Work",
+                    InWork = true,
+                },
+                new CallbackRequest()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    IsDeleted = false,
+
+                    AuthorEmail = "NotDeletedEmail@email.com",
+                    AuthorFirstName = "Test",
+                    AuthorLastName = "Test",
+                    AuthorPhone = "Test",
+                    Budget = 10000,
+                    Message = "I like tests!",
+                    IntrerestedIn = "Big project",
+                    InWork = true,
+                },
+                new CallbackRequest()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    IsDeleted = false,
+
+                    AuthorEmail = "NotDeletedEmail@email.com",
+                    AuthorFirstName = "Test",
+                    AuthorLastName = "Test",
+                    AuthorPhone = "Test",
+                    Budget = 10000,
+                    Message = "I like tests!",
+                    IntrerestedIn = "Big project",
+                    InWork = true,
+                });
+
+            databaseContext.SaveChanges();
+
+            var callbackRequestService = new CallbackRequestService(databaseContext);
+
+            //Act
+            var onlyNotDeleted = await callbackRequestService.GetAllCallbackRequestsAsync(false);
 
             //Assert
-            
+            onlyNotDeleted.DTO.Should().OnlyContain(c => !c.IsDeleted);
+        }
+
+
+
+        private MikartContext CreateContext()
+        {
+            var contextOptions = new DbContextOptionsBuilder<MikartContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new MikartContext(contextOptions);
+            return context;
         }
     }
 }
