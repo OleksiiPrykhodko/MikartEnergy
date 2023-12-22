c49338ebb28652c1e27ea8912b19c7fdc08714b7
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-12-22 09:46:41 +0200

    I have created folder for test fixtures and first class CallbackRequestsFixtures in this folder.

---
+++ backend/MikartEnergy.UnitTests/Fixtures/CallbackRequestsFixtures.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using MikartEnergy.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.UnitTests.Fixtures
+{
+    public class CallbackRequestsFixtures
+    {
+        public static List<CallbackRequest> GetCallbackRequests()
+        {
+            var callbackRequests = new List<CallbackRequest>()
+            {
+                new CallbackRequest()
+                {
+                    Id = Guid.NewGuid(),
+                    IsDeleted = true,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    AuthorEmail = "DeletedEmail@email.com",
+                    AuthorFirstName = "Test",
+                    AuthorLastName = "Test",
+                    AuthorPhone = "Test",
+                    Budget = 10000,
+                    Message = "I like tests!",
+                    IntrerestedIn = "Work",
+                    InWork = true,
+                },
+                new CallbackRequest()
+                {
+                    Id = Guid.NewGuid(),
+                    IsDeleted = false,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    AuthorEmail = "NotDeletedEmail@email.com",
+                    AuthorFirstName = "Test",
+                    AuthorLastName = "Test",
+                    AuthorPhone = "Test",
+                    Budget = 10000,
+                    Message = "I like tests!",
+                    IntrerestedIn = "Big project",
+                    InWork = true,
+                },
+                new CallbackRequest()
+                {
+                    Id = Guid.NewGuid(),
+                    IsDeleted = false,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    AuthorEmail = "NotDeletedEmail@email.com",
+                    AuthorFirstName = "Test",
+                    AuthorLastName = "Test",
+                    AuthorPhone = "Test",
+                    Budget = 10000,
+                    Message = "I like tests!",
+                    IntrerestedIn = "Big project",
+                    InWork = true,
+                }
+            };
+
+            return callbackRequests;
+        }
+    }
+}
