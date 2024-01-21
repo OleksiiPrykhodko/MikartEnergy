2b08c7712fee7849392cdaa952522bfd79e84cb4
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-01-21 16:08:03 +0200

    I've modified search-products-page.component.ts becouse it had a mistake. Now it is not standalone.

--- frontend/src/app/pages/shop-pages/search-products-page/search-products-page.component.ts
+++ frontend/src/app/pages/shop-pages/search-products-page/search-products-page.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-products-page',
-  standalone: true,
-  imports: [],
   templateUrl: './search-products-page.component.html',
   styleUrl: './search-products-page.component.scss'
 })
