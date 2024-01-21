c804c64a6732b1cf5c36907810d5a330a695e3c3
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-01-21 14:51:02 +0200

    I've created search-products-page component for displaying the search result.

---
+++ frontend/src/app/pages/shop-pages/search-products-page/search-products-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-products-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-products-page.component.html',
+  styleUrl: './search-products-page.component.scss'
+})
+export class SearchProductsPageComponent {
+
+}
