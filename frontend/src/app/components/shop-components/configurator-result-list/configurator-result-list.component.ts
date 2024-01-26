73650b68575d5715b020ca6c1bee821e07936442
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-01-26 12:22:49 +0200

    I've created new conponent for showing result received from TIA Selection Tool.

---
+++ frontend/src/app/components/shop-components/configurator-result-list/configurator-result-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configurator-result-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './configurator-result-list.component.html',
+  styleUrl: './configurator-result-list.component.scss'
+})
+export class ConfiguratorResultListComponent {
+
+}
