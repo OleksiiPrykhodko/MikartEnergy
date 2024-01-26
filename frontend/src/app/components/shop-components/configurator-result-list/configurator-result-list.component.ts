a78784f605f81dd0687ca5654f47fd17caab5db5
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-01-26 12:31:22 +0200

    I've added to the configurator-result-list all needed logic and layout from WideProductMinimalsComponent.

--- frontend/src/app/components/shop-components/configurator-result-list/configurator-result-list.component.ts
+++ frontend/src/app/components/shop-components/configurator-result-list/configurator-result-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { keyValuePair } from 'src/app/models/common/keyValuePair';
+import { ProductMinimal } from 'src/app/models/product/prodact-minimal';
 
 @Component({
   selector: 'app-configurator-result-list',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrl: './configurator-result-list.component.scss'
 })
 export class ConfiguratorResultListComponent {
+  @Input() _products: keyValuePair<ProductMinimal, number>[];
+  @Input() _unknownProducts: keyValuePair<keyValuePair<string, string>, number>[];
+  @Input() _title: string;
+
+  ngOnChanges(){
+    this._products.forEach(pair => 
+      {
+        // If imageLowQualityURL is empty then set special img.
+        if(!pair.key.imageLowQualityURL){
+          pair.key.imageLowQualityURL = "assets/images/ImgNotFound.svg";
+        }
+      });
+  }
+
+  public getProductMinimals(): keyValuePair<ProductMinimal, number>[] {
+    return this._products;
+  }
+
+  public getUnknownProduct(): keyValuePair<keyValuePair<string, string>, number>[]{
+    return this._unknownProducts;
+  }
+
+  public checkLoaded(): boolean{
+    return this._products.length > 0 || this._unknownProducts.length > 0;
+  }
+
+  public setLinkToProductPage(productId: string | null | undefined): string{
+    if(productId){
+      return `shop/products/${productId}`;
+    }
+    return "404";
+  }
+
+  public calculatePrice(price: number, quantity: number): number{
+    return price * quantity;
+  }
+
+  public getUnknownProductImg(): string{
+    return "assets/images/x-circle.svg";
+  }
 
 }
