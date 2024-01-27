423e25b72b5d9d0cf4fc819320935624c4218211
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-01-27 22:06:29 +0200

    I've regestered ReactiveFormsModule in the app.module.ts.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -60,6 +60,7 @@ import { ConfiguratorResultListComponent } from './components/shop-components/co
     HttpClientModule,
     RouterModule,
     FormsModule,
+    ReactiveFormsModule,
 
     ButtonModule,
     AutoCompleteModule,
