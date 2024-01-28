eb3d47e6e5f02bbc6d861ab48fc05953182f4a36
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-01-28 16:56:34 +0200

    I have added base logic and layout to the search-products-page.component.

--- frontend/src/app/pages/shop-pages/search-products-page/search-products-page.component.ts
+++ frontend/src/app/pages/shop-pages/search-products-page/search-products-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProductMinimal } from 'src/app/models/product/prodact-minimal';
+import { ProductService } from 'src/app/services/shop-service/product.service';
 
 @Component({
   selector: 'app-search-products-page',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './search-products-page.component.scss'
 })
 export class SearchProductsPageComponent {
+
+  private _subscriptionToRoutParamChange: Subscription;
+  private _searchedOrderNumberPart: string = "";
+  private _retrievedProducts: ProductMinimal[] = [];
+
+  constructor( 
+    private _activatedRoute: ActivatedRoute,
+    private _productService: ProductService) 
+  {
+    this._subscriptionToRoutParamChange = _activatedRoute.queryParams
+      .subscribe(params => this._searchedOrderNumberPart = params["orderNumber"] ?? "");
+  }
+
+  ngOnInit(){
+    
+  }
+
+  ngOnDestroy() {
+    this._subscriptionToRoutParamChange?.unsubscribe();
+  }
+
+  public getProducts(){
+    return this._retrievedProducts;
+  }
 
 }
