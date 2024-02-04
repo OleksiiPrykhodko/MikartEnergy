f1c14c3049391ceeac0ec9b8b70f78965a10a821
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-02-04 17:36:13 +0200

    I've added to the all methods of http-internal.service.ts call of http errors handler method.

--- frontend/src/app/services/http-internal.service.ts
+++ frontend/src/app/services/http-internal.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -30,39 +30,48 @@ export class HttpInternalService {
   }
 
   public getRequest<T>(url: string, httpQueryParams?: HttpParams): Observable<T> {
-    return this.http.get<T>(this.buildUrl(url), { headers: this.getHeaders(), params: httpQueryParams });
+    return this.http.get<T>(this.buildUrl(url), { headers: this.getHeaders(), params: httpQueryParams })
+      .pipe(catchError(this.handleError));
   }
 
   public getFullRequest<T>(url: string, httpQueryParams?: HttpParams): Observable<HttpResponse<T>> {
-    return this.http.get<T>(this.buildUrl(url), { observe: 'response', headers: this.getHeaders(), params: httpQueryParams });
+    return this.http.get<T>(this.buildUrl(url), { observe: 'response', headers: this.getHeaders(), params: httpQueryParams })
+      .pipe(catchError(this.handleError));
   }
 
   public postClearRequest<T>(url: string, payload: object): Observable<T> {
-    return this.http.post<T>(this.buildUrl(url), payload);
+    return this.http.post<T>(this.buildUrl(url), payload)
+      .pipe(catchError(this.handleError));
   }
 
   public postRequest<T>(url: string, payload: object): Observable<T> {
-    return this.http.post<T>(this.buildUrl(url), payload, { headers: this.getHeaders() });
+    return this.http.post<T>(this.buildUrl(url), payload, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
 
   public postFullRequest<T>(url: string, payload: object): Observable<HttpResponse<T>> {
-    return this.http.post<T>(this.buildUrl(url), payload, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.post<T>(this.buildUrl(url), payload, { headers: this.getHeaders(), observe: 'response' })
+      .pipe(catchError(this.handleError));
   }
 
   public putRequest<T>(url: string, payload: object): Observable<T> {
-    return this.http.put<T>(this.buildUrl(url), payload, { headers: this.getHeaders() });
+    return this.http.put<T>(this.buildUrl(url), payload, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
 
   public putFullRequest<T>(url: string, payload: object): Observable<HttpResponse<T>> {
-    return this.http.put<T>(this.buildUrl(url), payload, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.put<T>(this.buildUrl(url), payload, { headers: this.getHeaders(), observe: 'response' })
+      .pipe(catchError(this.handleError));
   }
 
   public deleteRequest<T>(url: string, httpQueryParams?: HttpParams): Observable<T> {
-    return this.http.delete<T>(this.buildUrl(url), { headers: this.getHeaders(), params: httpQueryParams });
+    return this.http.delete<T>(this.buildUrl(url), { headers: this.getHeaders(), params: httpQueryParams })
+      .pipe(catchError(this.handleError));
   }
 
   public deleteFullRequest<T>(url: string, httpQueryParams?: HttpParams): Observable<HttpResponse<T>> {
-    return this.http.delete<T>(this.buildUrl(url), { headers: this.getHeaders(), observe: 'response', params: httpQueryParams });
+    return this.http.delete<T>(this.buildUrl(url), { headers: this.getHeaders(), observe: 'response', params: httpQueryParams })
+      .pipe(catchError(this.handleError));
   }
 
   public buildUrl(url: string): string {
@@ -74,5 +83,20 @@ export class HttpInternalService {
 
   public prepareData(payload: object): string {
     return JSON.stringify(payload);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred.
+      console.error("Client-side or network error occurred.");
+      //TODO: Add here Call of pop up or toast with info about occurred error.
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body contain clues as to what went wrong.
+      console.error(`Backend returned code ${error.status}.`);
+      //TODO: Add here Call of pop up or toast with info - "Error occurred".
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(error);
   }
 }
