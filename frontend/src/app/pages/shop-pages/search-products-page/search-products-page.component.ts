c975d192af03b7d6f269a74e919405fe1a14a2fe
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-02-04 17:52:30 +0200

    I've added basic logic to the search-products-page.component.ts.

--- frontend/src/app/pages/shop-pages/search-products-page/search-products-page.component.ts
+++ frontend/src/app/pages/shop-pages/search-products-page/search-products-page.component.ts
@@ -1,5 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ProductMinimal } from 'src/app/models/product/prodact-minimal';
 import { ProductService } from 'src/app/services/shop-service/product.service';
@@ -12,23 +13,50 @@ import { ProductService } from 'src/app/services/shop-service/product.service';
 export class SearchProductsPageComponent {
 
   private _subscriptionToRoutParamChange: Subscription;
+  private _subscriptionToProductMinimals: Subscription;
   private _searchedOrderNumberPart: string = "";
   private _retrievedProducts: ProductMinimal[] = [];
+  private _infoIsLoaded: boolean = true; 
 
   constructor( 
     private _activatedRoute: ActivatedRoute,
     private _productService: ProductService) 
-  {
-    this._subscriptionToRoutParamChange = _activatedRoute.queryParams
-      .subscribe(params => this._searchedOrderNumberPart = params["orderNumber"] ?? "");
-  }
+  { }
 
   ngOnInit(){
+    this._subscriptionToRoutParamChange = this._activatedRoute.queryParams
+      .subscribe(params => {
+        this._searchedOrderNumberPart = params["orderNumber"] ?? "";
     
+        this._subscriptionToProductMinimals = this._productService
+          .getProductMinamalsByPartOfOrderNumber(this._searchedOrderNumberPart)
+          .subscribe(
+            result => { 
+              this._retrievedProducts = result?.body?.dto || [];
+              this._infoIsLoaded = false },
+            error => {
+              if(error instanceof HttpErrorResponse){
+                if(error.status === 0){
+                  console.log("Client-side or network error occurred");
+                }
+                else{
+                  console.log(`Server error: ${error.status}`);
+                }
+              }else{
+                console.log("Unexpected Error.")
+              }
+            });
+      });
+
   }
 
   ngOnDestroy() {
     this._subscriptionToRoutParamChange?.unsubscribe();
+    this._subscriptionToProductMinimals?.unsubscribe();
+  }
+
+  public checkLoading(): boolean {
+    return this._infoIsLoaded;
   }
 
   public getProducts(){
