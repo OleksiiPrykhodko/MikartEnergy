7052156ec00ac285bb15a8692ca64269e840d78c
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-04-05 18:35:43 +0300

    I've created class ProductMinimalsQueryParams based on PaginationQueryParams for receiving products filtering and pagination params from query string.

---
+++ backend/MikartEnergy.Common/QueryParams/Product/ProductMinimalsQueryParams.cs
@@ -0,0 +1,17 @@
+﻿using MikartEnergy.Common.QueryParams.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.Common.QueryParams.Product
+{
+    public class ProductMinimalsQueryParams : PaginationQueryParams
+    {
+        public string? OrderNumber { get; set; } = null;
+        public string? ProductName { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDescending { get; set; } = false;
+    }
+}
