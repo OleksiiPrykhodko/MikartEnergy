1714ad571415d3509a56df0f38c8d53bdfc96b43
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2024-04-05 18:33:04 +0300

    I've created abstract class PaginationQueryParams for getting pagination parametrs from query params.

---
+++ backend/MikartEnergy.Common/QueryParams/Pagination/PaginationQueryParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikartEnergy.Common.QueryParams.Pagination
+{
+    public abstract class PaginationQueryParams
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
